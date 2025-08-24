f31b85e0a106553dad582530fa6538067f83bdc2
MyoLinTun <mgmyolintun@gmail.com>
2025-08-24 19:24:53 +0630

    types and opeators

---
+++ 1.type_and_operations.py
@@ -0,0 +1,31 @@
+# type of integers (int/float);
+
+print(type (5)); #int
+print(type (2.5)); #float
+
+
+# Operations
+
+# addition +
+# subtraction -
+# multiplication *
+# division /
+# exponent **
+# remainder %
+# integer division //
+
+print(5//3) #1(quotient)
+print(5%3) #2(remainder)
+
+# Order of operation (BODMAS)
+# Addition assignment +=
+# Subtraction assignment -=
+# multiplication assignment *=
+# division assignment /=
+
+# Variable declaration
+
+# var_name = value
+
+name = "aungaung";
+print(name);
